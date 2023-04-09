48ff935b38dc7e8f333e3bbd1a98563425dc4982
swindle627 <jdpackers544@gmail.com>
2023-04-08 23:21:44 -0400

    Heuristic and DFS

--- SearchAlgs/Program.cs
+++ SearchAlgs/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
-class SearchAlgs
+class SearchAlgorithms
 {
     static public void Main(String[] args)
     {
@@ -10,90 +12,280 @@ class SearchAlgs
          * 7 6 5
          */
         int[,] goalState = { { 1, 2, 3 }, { 8, 0, 4 }, { 7, 6, 5 } };
-        int timesToRun = 20; // number of times the algorithms will run in while loop
-        int[,] initialState = new int[3, 3]; // Initial Puzzle
+        // the amount of solvable puzzles the algorithms will solve
+        int puzzleCount = 1;
 
-        while(timesToRun != 0)
+        RunAlgs(puzzleCount, goalState);
+
+    }
+    // Runs the various search algorithms
+    public static void RunAlgs(int puzzleCount, int[,] goalState)
+    {
+        Tuple<int, int>[] goalPos = new Tuple<int, int>[goalState.Length];
+        int[] goalStateLin = ConvertToLinear(goalState, ref goalPos);
+
+        while(puzzleCount != 0)
         {
-            initialState = GeneratePuzzle(initialState);
+            Tuple<int, int> emptyPos = new Tuple<int, int>(1, 1);
+            int heuristicVal;
+            int[,] initialState = { { 1, 3, 4 }, { 8, 0, 2 }, { 7, 6, 5 } };
+            int[,] dfsArray = new int[goalState.GetLength(0), goalState.GetLength(1)];
+            int[] initialStateLin;
 
-            Console.WriteLine("Goal State");
-            DisplayPuzzle(goalState);
+            initialStateLin = ConvertToLinear(initialState, goalPos);
+            PuzzleSolvable(initialStateLin);
+            heuristicVal = CalculateHeuristic(initialState, goalState, goalPos);
 
-            if(IsSolvable(initialState))
+            //do
+            //{
+            //    initialState = GeneratePuzzle(goalState.GetLength(0), goalState.GetLength(1), goalStateLin, ref emptyPos);
+            //    initialStateLin = ConvertToLinear(initialState, goalPos);
+            //}
+            //while(!PuzzleSolvable(initialStateLin));
+
+            Array.Copy(initialState, dfsArray, initialState.Length);
+            bool solved = DepthFirstSearch(dfsArray, null, goalState, emptyPos, 0, heuristicVal);
+
+            DisplayPuzzle(initialState, goalState);
+            Console.WriteLine(heuristicVal);
+            Console.WriteLine("Puzzle solved: " + solved);
+            puzzleCount--;
+        }
+    }
+    // Performs recursive DFS search
+    public static bool DepthFirstSearch(int[,] currState, int[,] prevState, int[,] goalState, Tuple<int, int> emptyPos, int currDepth, int depthLimit)
+    {
+        bool goalReached = false;
+
+        //DisplayPuzzle(currState, goalState);
+        // i actually need to check to see if the state im about to change to equals the parent's parent
+        // aka prevPrevState
+
+        if (currState == goalState)
+        {
+            goalReached = true;
+            return goalReached;
+        }
+
+        if(currDepth != depthLimit)
+        {
+            bool upValid, downValid, leftValid, rightValid;
+
+            if(currDepth == 0)
             {
-                Console.WriteLine("Initial State");
+                upValid = (emptyPos.Item1 - 1 >= 0);
+                downValid = (emptyPos.Item1 + 1 < currState.GetLength(0));
+                leftValid = (emptyPos.Item2 - 1 >= 0);
+                rightValid = (emptyPos.Item2 + 1 < currState.GetLength(1));
             }
             else
             {
-                Console.WriteLine("Puzzle not solvable");
+                upValid = (emptyPos.Item1 - 1 >= 0) && (currState != prevState);
+                downValid = (emptyPos.Item1 + 1 < currState.GetLength(0)) && (currState != prevState);
+                leftValid = (emptyPos.Item2 - 1 >= 0) && (currState != prevState);
+                rightValid = (emptyPos.Item2 + 1 < currState.GetLength(1)) && (currState != prevState);
+                DisplayPuzzle(currState, prevState);
             }
-            
-            DisplayPuzzle(initialState);
 
-            timesToRun--;
+            currDepth += 1;
+
+            if (leftValid && !goalReached)
+            {
+                Console.WriteLine("Left " + currDepth);
+                Tuple<int, int> leftEmptyPos = new Tuple<int, int>(emptyPos.Item1, emptyPos.Item2 - 1);
+                int[,] leftState = SwapValues(currState, leftEmptyPos, emptyPos);
+                goalReached = DepthFirstSearch(leftState, currState, goalState, leftEmptyPos, currDepth, depthLimit);
+            }
+
+            if (rightValid && !goalReached)
+            {
+                Console.WriteLine("Right " + currDepth);
+                Tuple<int, int> rightEmptyPos = new Tuple<int, int>(emptyPos.Item1, emptyPos.Item2 + 1);
+                int[,] rightState = SwapValues(currState, rightEmptyPos, emptyPos);
+                goalReached = DepthFirstSearch(rightState, currState, goalState, rightEmptyPos, currDepth, depthLimit);
+            }
+
+            if (upValid && !goalReached)
+            {
+                Console.WriteLine("Up " + currDepth);
+                Tuple<int, int> upEmptyPos = new Tuple<int, int>(emptyPos.Item1 - 1, emptyPos.Item2);
+                int[,] upState = SwapValues(currState, upEmptyPos, emptyPos);
+                goalReached = DepthFirstSearch(upState, currState, goalState, upEmptyPos, currDepth, depthLimit);
+            }
+
+            if (downValid && !goalReached)
+            {
+                Console.WriteLine("Down " + currDepth);
+                Tuple<int, int> downEmptyPos = new Tuple<int, int>(emptyPos.Item1 + 1, emptyPos.Item2);
+                int[,] downState = SwapValues(currState, downEmptyPos, emptyPos);
+                goalReached = DepthFirstSearch(downState, currState, goalState, downEmptyPos, currDepth, depthLimit);
+            }
+        }
+        
+        return goalReached;
+    }
+    // Calculates the heuristic value for the entered state
+    public static int CalculateHeuristic(int[,] initialState, int[,] goalState, Tuple<int, int>[] goalPos)
+    {
+        int distance = 0;
+        int reversals = 0;
+
+        for(int i = 0; i < initialState.GetLength(0); i++)
+        {
+            for(int j = 0; j < initialState.GetLength(1); j++)
+            {
+                int value = initialState[i, j];
+
+                if(value != 0)
+                {
+                    int index = value - 1;
+                    int diff = Math.Abs(i - goalPos[index].Item1);
+                    diff += Math.Abs(j - goalPos[index].Item2);
+                    distance += diff;
+
+                    if(diff != 0)
+                    {
+                        // left reversal check
+                        if (j > 0 && initialState[i, j] == goalState[i, j - 1] && goalState[i, j] == initialState[i, j - 1])
+                        {
+                            reversals++;
+                            //Console.WriteLine("left running");
+                        }
+                        // right reversal check
+                        else if (j < initialState.GetLength(1) - 1 && initialState[i, j] == goalState[i, j + 1] && goalState[i, j] == initialState[i, j + 1])
+                        {
+                            reversals++;
+                            //Console.WriteLine("right running");
+                        }
+                        // up reversal check
+                        else if (i > 0 && initialState[i, j] == goalState[i - 1, j] && goalState[i, j] == initialState[i - 1, j])
+                        {
+                            reversals++;
+                            //Console.WriteLine("up running");
+                        }
+                        // down reversal check
+                        else if (i < initialState.GetLength(0) - 1 && initialState[i, j] == goalState[i + 1, j] && goalState[i, j] == initialState[i + 1, j])
+                        {
+                            reversals++;
+                            //Console.WriteLine("down running");
+                        }
+                    }
+                }
+            }
         }
 
+        //Console.WriteLine(distance);
+        //Console.WriteLine(reversals);
+        return (distance + (2 * reversals));
+    }
+    // Swaps the two index values entered and returns the array
+    public static int[,] SwapValues(int[,] currState, Tuple<int, int> val1, Tuple<int, int> val2)
+    {
+        int[,] newState = new int[currState.GetLength(0), currState.GetLength(1)];
+        Array.Copy(currState, newState, currState.Length);
+        int temp = newState[val1.Item1, val1.Item2];
+        newState[val1.Item1, val1.Item2] = newState[val2.Item1, val2.Item2];
+        newState[val2.Item1, val2.Item2] = temp;
+        return newState;
     }
     // Randomly generates an initial state for the puzzle and returns it
-    public static int[,] GeneratePuzzle(int[,] initialSate)
+    // Height and width are the dimensions of the goal puzzle
+    // numsToAdd is an array of numbers that are in the goal puzzle
+    public static int[,] GeneratePuzzle(int height, int width, int[] numsToAdd, ref Tuple<int,int> emptyPos)
     {
-        List<int> numsToAdd = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[,] initialState = new int[height, width];
         Random rnd = new Random();
+        List<int> nums = new List<int>(numsToAdd);
 
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < height; i++)
         {
-            for(int j = 0; j < 3; j++)
+            for (int j = 0; j < width; j++)
             {
-                int index = rnd.Next(0, numsToAdd.Count);
-                initialSate[i, j] = numsToAdd[index];
-                numsToAdd.RemoveAt(index);
+                int index = rnd.Next(0, nums.Count);
+                initialState[i, j] = nums[index];
+                if (nums[index] == 0)
+                {
+                    emptyPos = new Tuple<int, int>(i, j);
+                }
+                nums.RemoveAt(index);
             }
         }
 
-        return initialSate;
+        return initialState;
     }
     // Formats and displays the puzzle entered
-    public static void DisplayPuzzle(int[,] puzzle)
+    public static void DisplayPuzzle(int[,] initialPuzzle, int[,] goalPuzzle)
     {
-        string display = "";
-
-        for (int i = 0; i < 3; i++)
+        string display = "Initial Puzzle\tGoal Puzzle\n";
+        display += "------------\t------------\n";
+        
+        for (int i = 0; i < initialPuzzle.GetLength(0); i++)
         {
-            for(int j = 0; j < 3; j++)
-            {
-                display += " " + puzzle[i, j] + " ";
+            string initialDisplay = "";
+            string goalDisplay = "";
 
-                if(j != 2)
+            for(int j = 0; j < initialPuzzle.GetLength(1); j++)
+            {
+                initialDisplay += " " + initialPuzzle[i, j] + " ";
+                goalDisplay += " " + goalPuzzle[i, j] + " ";
+
+                if(j != initialPuzzle.GetLength(1) - 1)
                 {
-                    display += "|";
+                    initialDisplay += "|";
+                    goalDisplay += "|";
                 }
             }
 
-            if (i != 2)
+            display += initialDisplay + "\t" + goalDisplay + "\n";
+            display += "------------\t------------\n";
+        }
+
+        Console.WriteLine(display + "\n");
+    }
+    // Converts goal puzzle from 2d array to 1d array
+    // Sets up an array for the goal positions of each tile
+    public static int[] ConvertToLinear(int[,] goalPuzzle, ref Tuple<int, int>[] goalPos)
+    {
+        int[] linForm = new int[goalPuzzle.Length];
+
+        for (int i = 0; i < goalPuzzle.GetLength(0); i++)
+        {
+            for (int j = 0; j < goalPuzzle.GetLength(1); j++)
             {
-                display += "\n------------\n";
+                int index;
+
+                if (goalPuzzle[i, j] == 0)
+                {
+                    index = goalPuzzle.Length - 1;
+                }
+                else
+                {
+                    index = goalPuzzle[i, j] - 1;
+                }
+
+                goalPos[index] = Tuple.Create(i, j);
+                linForm[index] = goalPuzzle[i, j];
             }
         }
 
-        display += "\n\n";
-        Console.WriteLine(display);
+        return linForm;
+    }
+    // Converts initial puzzle from 2d array to 1d array
+    public static int[] ConvertToLinear(int[,] initialPuzzle, Tuple<int, int>[] goalPos)
+    {
+        int[] linForm = new int[initialPuzzle.Length];
+
+        for(int i = 0; i < linForm.Length; i++)
+        {
+            linForm[i] = initialPuzzle[goalPos[i].Item1, goalPos[i].Item2];
+        }
+
+        return linForm;
     }
     // Checks if the puzzle is solvable
+    // Counts inversions in the puzzle and returns false if inversion count is odd
     // Odd inversion count == not solvable
-    public static bool IsSolvable(int[,] initialState)
-    {
-        int[] linForm =
-        {
-            initialState[0,0], initialState[0,1], initialState[0,2],
-            initialState[1,2], initialState[2,2], initialState[2,1],
-            initialState[2,0], initialState[1,0], initialState[1,1]
-        };
-
-        return (CountInversions(linForm) % 2 == 0);
-    }
-    // Counts inversions of the array entered
-    public static int CountInversions(int[] puzzleLinForm)
+    public static bool PuzzleSolvable(int[] puzzleLinForm)
     {
         int count = 0;
 
@@ -108,6 +300,7 @@ class SearchAlgs
             }
         }
 
-        return count;
+        //Console.WriteLine(count);
+        return (count % 2 == 0);
     }
 }
