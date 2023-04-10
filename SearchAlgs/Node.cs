ce0b49df850949762ec0f0b4d3d1bf692e45c50c
swindle <jdpackers544@gmail.com>
2023-04-09 23:12:08 -0400

    DFS complete

--- SearchAlgs/Node.cs
+++ SearchAlgs/Node.cs
@@ -8,5 +8,32 @@ namespace SearchAlgs
 {
     internal class Node
     {
+        public int[,] state;
+        public Tuple<int, int> emptyPos;
+        public Node? left, right, up, down;
+        public Node? parent;
+
+        public Node(int[,] state, Tuple<int, int> emptyPos)
+        {
+            this.state = new int[state.GetLength(0), state.GetLength(1)];
+            Array.Copy(state, this.state, state.Length);
+            this.emptyPos = emptyPos;
+            parent = null;
+            left = null;
+            right = null;
+            up= null;
+            down = null;
+        }
+        public Node(int[,] state, Tuple<int, int> emptyPos, Node parent)
+        {
+            this.state = new int[state.GetLength(0), state.GetLength(1)];
+            Array.Copy(state, this.state, state.Length);
+            this.emptyPos = emptyPos;
+            this.parent = parent;
+            left = null;
+            right = null;
+            up = null;
+            down = null;
+        }
     }
 }
--- SearchAlgs/Program.cs
+++ SearchAlgs/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SearchAlgs;
+using System;
 using System.Numerics;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 class SearchAlgorithms
 {
@@ -12,56 +15,65 @@ class SearchAlgorithms
          * 7 6 5
          */
         int[,] goalState = { { 1, 2, 3 }, { 8, 0, 4 }, { 7, 6, 5 } };
-        // the amount of solvable puzzles the algorithms will solve
-        int puzzleCount = 1;
 
-        RunAlgs(puzzleCount, goalState);
+        // The amount of solvable puzzles the algorithms will solve
+        int puzzleCount = 20;
 
+        // Depth limit for DFS
+        // Max moves needed to solve hardest 8-puzzle is 31 (80 for 15-puzzle)
+        int depthLimit = 31;
+
+        RunAlgs(puzzleCount, goalState, depthLimit);
     }
     // Runs the various search algorithms
-    public static void RunAlgs(int puzzleCount, int[,] goalState)
+    public static void RunAlgs(int puzzleCount, int[,] goalState, int depthLimit)
     {
         Tuple<int, int>[] goalPos = new Tuple<int, int>[goalState.Length];
+        Tuple<long, int>[] dfsStats = new Tuple<long, int>[puzzleCount];
+        
         int[] goalStateLin = ConvertToLinear(goalState, ref goalPos);
 
         while(puzzleCount != 0)
         {
-            Tuple<int, int> emptyPos = new Tuple<int, int>(1, 1);
+            Tuple<int, int> emptyPos = new Tuple<int, int>(0, 0);
+            Stopwatch watch = new Stopwatch();
             int heuristicVal;
-            int[,] initialState = { { 1, 3, 4 }, { 8, 0, 2 }, { 7, 6, 5 } };
-            int[,] dfsArray = new int[goalState.GetLength(0), goalState.GetLength(1)];
+            int[,] initialState;
             int[] initialStateLin;
+            int nodesVisited = 0;
 
-            initialStateLin = ConvertToLinear(initialState, goalPos);
-            PuzzleSolvable(initialStateLin);
+            do
+            {
+                initialState = GeneratePuzzle(goalState.GetLength(0), goalState.GetLength(1), goalStateLin, ref emptyPos);
+                initialStateLin = ConvertToLinear(initialState, goalPos);
+            }
+            while (!PuzzleSolvable(initialStateLin));
+
             heuristicVal = CalculateHeuristic(initialState, goalState, goalPos);
+            //Console.WriteLine(heuristicVal);
 
-            //do
-            //{
-            //    initialState = GeneratePuzzle(goalState.GetLength(0), goalState.GetLength(1), goalStateLin, ref emptyPos);
-            //    initialStateLin = ConvertToLinear(initialState, goalPos);
-            //}
-            //while(!PuzzleSolvable(initialStateLin));
+            Node root = new Node(initialState, emptyPos);
+            watch.Start();
+            bool dfsSolved = DepthFirstSearch(root, goalState, 0, depthLimit, ref nodesVisited);
+            watch.Stop();
+            dfsStats[puzzleCount - 1] = Tuple.Create(watch.ElapsedMilliseconds, nodesVisited);
+            Console.WriteLine(nodesVisited);
+            watch.Reset();
+            nodesVisited = 0;
 
-            Array.Copy(initialState, dfsArray, initialState.Length);
-            bool solved = DepthFirstSearch(dfsArray, null, goalState, emptyPos, 0, heuristicVal);
-
-            DisplayPuzzle(initialState, goalState);
-            Console.WriteLine(heuristicVal);
-            Console.WriteLine("Puzzle solved: " + solved);
+            DisplayPuzzle(initialState, goalState, dfsSolved);
             puzzleCount--;
         }
+
+        DisplayStats(dfsStats);
     }
     // Performs recursive DFS search
-    public static bool DepthFirstSearch(int[,] currState, int[,] prevState, int[,] goalState, Tuple<int, int> emptyPos, int currDepth, int depthLimit)
+    public static bool DepthFirstSearch(Node currNode, int[,] goalState, int currDepth, int depthLimit, ref int nodesVisited)
     {
         bool goalReached = false;
+        nodesVisited++;
 
-        //DisplayPuzzle(currState, goalState);
-        // i actually need to check to see if the state im about to change to equals the parent's parent
-        // aka prevPrevState
-
-        if (currState == goalState)
+        if (CompareArrays(currNode.state, goalState))
         {
             goalReached = true;
             return goalReached;
@@ -69,56 +81,87 @@ class SearchAlgorithms
 
         if(currDepth != depthLimit)
         {
-            bool upValid, downValid, leftValid, rightValid;
-
-            if(currDepth == 0)
-            {
-                upValid = (emptyPos.Item1 - 1 >= 0);
-                downValid = (emptyPos.Item1 + 1 < currState.GetLength(0));
-                leftValid = (emptyPos.Item2 - 1 >= 0);
-                rightValid = (emptyPos.Item2 + 1 < currState.GetLength(1));
-            }
-            else
-            {
-                upValid = (emptyPos.Item1 - 1 >= 0) && (currState != prevState);
-                downValid = (emptyPos.Item1 + 1 < currState.GetLength(0)) && (currState != prevState);
-                leftValid = (emptyPos.Item2 - 1 >= 0) && (currState != prevState);
-                rightValid = (emptyPos.Item2 + 1 < currState.GetLength(1)) && (currState != prevState);
-                DisplayPuzzle(currState, prevState);
-            }
+            bool upValid = (currNode.emptyPos.Item1 - 1 >= 0);
+            bool downValid = (currNode.emptyPos.Item1 + 1 < currNode.state.GetLength(0));
+            bool leftValid = (currNode.emptyPos.Item2 - 1 >= 0);
+            bool rightValid = (currNode.emptyPos.Item2 + 1 < currNode.state.GetLength(1));
 
             currDepth += 1;
 
             if (leftValid && !goalReached)
             {
-                Console.WriteLine("Left " + currDepth);
-                Tuple<int, int> leftEmptyPos = new Tuple<int, int>(emptyPos.Item1, emptyPos.Item2 - 1);
-                int[,] leftState = SwapValues(currState, leftEmptyPos, emptyPos);
-                goalReached = DepthFirstSearch(leftState, currState, goalState, leftEmptyPos, currDepth, depthLimit);
+                Tuple<int, int> leftEmptyPos = new Tuple<int, int>(currNode.emptyPos.Item1, currNode.emptyPos.Item2 - 1);
+                int[,] leftState = SwapValues(currNode.state, leftEmptyPos, currNode.emptyPos);
+                currNode.left = new Node(leftState, leftEmptyPos, currNode);
+
+                if (currNode.parent != null)
+                {
+                    if(!CompareArrays(currNode.left.state, currNode.parent.state))
+                    {
+                        goalReached = DepthFirstSearch(currNode.left, goalState, currDepth, depthLimit, ref nodesVisited);
+                    }
+                }
+                else
+                {
+                    goalReached = DepthFirstSearch(currNode.left, goalState, currDepth, depthLimit, ref nodesVisited);
+                }
             }
 
             if (rightValid && !goalReached)
             {
-                Console.WriteLine("Right " + currDepth);
-                Tuple<int, int> rightEmptyPos = new Tuple<int, int>(emptyPos.Item1, emptyPos.Item2 + 1);
-                int[,] rightState = SwapValues(currState, rightEmptyPos, emptyPos);
-                goalReached = DepthFirstSearch(rightState, currState, goalState, rightEmptyPos, currDepth, depthLimit);
+                Tuple<int, int> rightEmptyPos = new Tuple<int, int>(currNode.emptyPos.Item1, currNode.emptyPos.Item2 + 1);
+                int[,] rightState = SwapValues(currNode.state, rightEmptyPos, currNode.emptyPos);
+                currNode.right = new Node(rightState, rightEmptyPos, currNode);
+
+                if (currNode.parent != null)
+                {
+                    if (!CompareArrays(currNode.right.state, currNode.parent.state))
+                    {
+                        goalReached = DepthFirstSearch(currNode.right, goalState, currDepth, depthLimit, ref nodesVisited);
+                    }
+                }
+                else
+                {
+                    goalReached = DepthFirstSearch(currNode.right, goalState, currDepth, depthLimit, ref nodesVisited);
+                }
             }
 
             if (upValid && !goalReached)
             {
-                Console.WriteLine("Up " + currDepth);
-                Tuple<int, int> upEmptyPos = new Tuple<int, int>(emptyPos.Item1 - 1, emptyPos.Item2);
-                int[,] upState = SwapValues(currState, upEmptyPos, emptyPos);
-                goalReached = DepthFirstSearch(upState, currState, goalState, upEmptyPos, currDepth, depthLimit);
+                Tuple<int, int> upEmptyPos = new Tuple<int, int>(currNode.emptyPos.Item1 - 1, currNode.emptyPos.Item2);
+                int[,] upState = SwapValues(currNode.state, upEmptyPos, currNode.emptyPos);
+                currNode.up = new Node(upState, upEmptyPos, currNode);
+
+                if (currNode.parent != null)
+                {
+                    if (!CompareArrays(currNode.up.state, currNode.parent.state))
+                    {
+                        goalReached = DepthFirstSearch(currNode.up, goalState, currDepth, depthLimit, ref nodesVisited);
+                    }
+                }
+                else
+                {
+                    goalReached = DepthFirstSearch(currNode.up, goalState, currDepth, depthLimit, ref nodesVisited);
+                }
             }
 
             if (downValid && !goalReached)
             {
-                Console.WriteLine("Down " + currDepth);
-                Tuple<int, int> downEmptyPos = new Tuple<int, int>(emptyPos.Item1 + 1, emptyPos.Item2);
-                int[,] downState = SwapValues(currState, downEmptyPos, emptyPos);
-                goalReached = DepthFirstSearch(downState, currState, goalState, downEmptyPos, currDepth, depthLimit);
+                Tuple<int, int> downEmptyPos = new Tuple<int, int>(currNode.emptyPos.Item1 + 1, currNode.emptyPos.Item2);
+                int[,] downState = SwapValues(currNode.state, downEmptyPos, currNode.emptyPos);
+                currNode.down = new Node(downState, downEmptyPos, currNode);
+
+                if (currNode.parent != null)
+                {
+                    if (!CompareArrays(currNode.down.state, currNode.parent.state))
+                    {
+                        goalReached = DepthFirstSearch(currNode.down, goalState, currDepth, depthLimit, ref nodesVisited);
+                    }
+                }
+                else
+                {
+                    goalReached = DepthFirstSearch(currNode.down, goalState, currDepth, depthLimit, ref nodesVisited);
+                }
             }
         }
         
@@ -149,25 +192,25 @@ class SearchAlgorithms
                         if (j > 0 && initialState[i, j] == goalState[i, j - 1] && goalState[i, j] == initialState[i, j - 1])
                         {
                             reversals++;
-                            //Console.WriteLine("left running");
+                            //Console.WriteLine("Direct Reversal: " + initialState[i, j] + ", " + initialState[i, j - 1]);
                         }
                         // right reversal check
                         else if (j < initialState.GetLength(1) - 1 && initialState[i, j] == goalState[i, j + 1] && goalState[i, j] == initialState[i, j + 1])
                         {
                             reversals++;
-                            //Console.WriteLine("right running");
+                            //Console.WriteLine("Direct Reversal: " + initialState[i, j] + ", " + initialState[i, j + 1]);
                         }
                         // up reversal check
                         else if (i > 0 && initialState[i, j] == goalState[i - 1, j] && goalState[i, j] == initialState[i - 1, j])
                         {
                             reversals++;
-                            //Console.WriteLine("up running");
+                            //Console.WriteLine("Direct Reversal: " + initialState[i, j] + ", " + initialState[i - 1, j]);
                         }
                         // down reversal check
                         else if (i < initialState.GetLength(0) - 1 && initialState[i, j] == goalState[i + 1, j] && goalState[i, j] == initialState[i + 1, j])
                         {
                             reversals++;
-                            //Console.WriteLine("down running");
+                            //Console.WriteLine("Direct Reversal: " + initialState[i, j] + ", " + initialState[i + 1, j]);
                         }
                     }
                 }
@@ -176,7 +219,24 @@ class SearchAlgorithms
 
         //Console.WriteLine(distance);
         //Console.WriteLine(reversals);
+        //Console.WriteLine(distance + reversals);
         return (distance + (2 * reversals));
+    }
+    // Compares two 2d arrays for equality
+    public static bool CompareArrays(int[,] arr1, int[,] arr2)
+    {
+        for(int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr1.GetLength(1); j++)
+            {
+                if (arr1[i, j] != arr2[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
     // Swaps the two index values entered and returns the array
     public static int[,] SwapValues(int[,] currState, Tuple<int, int> val1, Tuple<int, int> val2)
@@ -214,7 +274,7 @@ class SearchAlgorithms
         return initialState;
     }
     // Formats and displays the puzzle entered
-    public static void DisplayPuzzle(int[,] initialPuzzle, int[,] goalPuzzle)
+    public static void DisplayPuzzle(int[,] initialPuzzle, int[,] goalPuzzle, bool dfsSolved)
     {
         string display = "Initial Puzzle\tGoal Puzzle\n";
         display += "------------\t------------\n";
@@ -240,7 +300,25 @@ class SearchAlgorithms
             display += "------------\t------------\n";
         }
 
+        display += "DFS Solved: " + dfsSolved;
         Console.WriteLine(display + "\n");
+    }
+    // Displays algorithm statistics at the end of the program
+    public static void DisplayStats(Tuple<long, int>[] dfsStats)
+    {
+        double dfsNodeAvg = 0, dfsTimeAvg = 0;
+
+        for(int i = 0; i < dfsStats.Length; i++)
+        {
+            dfsTimeAvg += dfsStats[i].Item1;
+            dfsNodeAvg += dfsStats[i].Item2;
+        }
+
+        dfsTimeAvg = Math.Round(dfsTimeAvg / dfsStats.Length, 2);
+        dfsNodeAvg = Math.Round(dfsNodeAvg / dfsStats.Length, 2);
+
+        Console.WriteLine("DFS Runtime Average: " + dfsTimeAvg);
+        Console.WriteLine("DFS Node Visit Average: " + dfsNodeAvg);
     }
     // Converts goal puzzle from 2d array to 1d array
     // Sets up an array for the goal positions of each tile
@@ -284,14 +362,15 @@ class SearchAlgorithms
     }
     // Checks if the puzzle is solvable
     // Counts inversions in the puzzle and returns false if inversion count is odd
-    // Odd inversion count == not solvable
+    // If an odd board size has an odd number of inversions it isn't solvable
+    // 8-puzzle has 9! total configurations and 9!/2 or 181440 of them are solvable
     public static bool PuzzleSolvable(int[] puzzleLinForm)
     {
         int count = 0;
 
-        for(int i = 0; i < puzzleLinForm.Length; i++)
+        for (int i = 0; i < puzzleLinForm.Length; i++)
         {
-            for(int j = i + 1; j < puzzleLinForm.Length; j++)
+            for (int j = i + 1; j < puzzleLinForm.Length; j++)
             {
                 if (puzzleLinForm[i] > 0 && puzzleLinForm[j] > 0 && puzzleLinForm[i] > puzzleLinForm[j])
                 {
@@ -301,6 +380,7 @@ class SearchAlgorithms
         }
 
         //Console.WriteLine(count);
+        //Console.WriteLine(count % 2);
         return (count % 2 == 0);
     }
 }
